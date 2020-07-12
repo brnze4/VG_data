ae148b76cf5fd82803fb008e97d1e6342ae64199
brnze4 <67944559+brnze4@users.noreply.github.com>
2020-07-12 04:04:22 -0400

    Create single_url_scrape.py

---
+++ single_url_scrape.py
@@ -0,0 +1,9 @@
+'''This call will only scrape a URL for all images. It should
+        Attempt to find high resolution versions, within reason
+        if not, download the version shown
+    It will not
+        crawl page to page to gather images
+'''
+class single_url_scrape:
+    def __init__(self):
+      pass
